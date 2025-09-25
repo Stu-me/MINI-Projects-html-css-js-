73bfc4422cd17a5bc2d272059daa9856de2b46b6
Stu-me <stusundram@gmail.com>
2025-09-25 20:35:12 +0530

    its is a second commit on bg.color

--- bg.color.switch/script.js
+++ bg.color.switch/script.js
@@ -1,22 +1,28 @@
-const buttons = document.querySelectorAll('.button');
+const buttons = document.querySelectorAll('.button');// now we have all the buttons as nodelist( and not array)
 const body = document.querySelector('body');
 
-buttons.forEach(function(button){
-    console.log(button);
-    button.addEventListener('click',function(e){
+buttons.forEach(function(button){ // now we want to access each nodelist to check to whom it will be matched
+    console.log(button);         // also the thing is that nodelist cant take for therefore we have to do it in foreach
+    button.addEventListener('click',function(e){ // e is something the js api will do its like a blackbox for now 
         console.log(e);
         console.log(e.target);
         if(e.target.id === 'red'){
             body.style.backgroundColor = e.target.id;
         }
-        if(e.target.id === 'green'){
+        else if(e.target.id === 'green'){
             body.style.backgroundColor = e.target.id;
         }
-         if(e.target.id === 'blue'){
+        else if(e.target.id === 'blue'){
             body.style.backgroundColor = e.target.id;
         }
-         if(e.target.id === 'default'){
-            body.style.backgroundColor ="black";
+        else if(e.target.id === 'yellow'){
+            body.style.backgroundColor = e.target.id;
+        }
+        else if(e.target.id === 'orange'){
+            body.style.backgroundColor = e.target.id;
+        }
+        else if(e.target.id === 'default'){
+            body.style.backgroundColor = "black";
         }
     }); 
 });
