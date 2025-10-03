898ffb52d0a8fbffcb5b06f850ab1a0340080e51
Stu-me <stusundram@gmail.com>
2025-10-03 19:37:56 +0530

    end added -- start need attention

--- guess.the.number/script.chai.js
+++ guess.the.number/script.chai.js
@@ -1,22 +1,24 @@
-let randomNum = (Math.random()*100)+1;
+let randomNum = Math.trunc(Math.random()*100)+1;
 // const submit = document.querySelector('.form');
 const guessedVal = document.querySelector('#guessField');
 const submit = document.querySelector('#subt');
-const resultArea = document.querySelector('.resultParas');
+const startOver = document.querySelector('.resultParas');
 const prevGuessOutput = document.querySelector('.guesses');
 const leftGuessCount = document.querySelector('.remains');
 const message = document.querySelector('.lowOrHi')
 const errBox = document.querySelector('#errBox');
+// const errorBox = document.querySelector('#alert')
 
 const p = document.createElement('p');// creates a new element that is still in the memory only 
 
 let prevGuess = [];
 let numGuess = 10;
-let palyGame = true;
+let playGame = true;
 
-if(palyGame){
+if(playGame){
     submit.addEventListener('click',(e)=>{
         e.preventDefault();
+        if(!playGame) return;
         const guess = parseInt(guessedVal.value);
         validateInput(guess);
     });
@@ -35,13 +37,13 @@ function validateInput(value){
     else{
         // verified that the input is num and valid 
         prevGuess.push(value);
-        if(numGuess === 11){
-            resultOut(value);
+        numGuess--;
+        resultOut(value);
+        if(numGuess === 0){
             displayMessage(`Game over.Random number was ${randomNum}`);
             endGame();
         }
         else{
-            resultOut(value);
             checkGuess(value);
         }
     }
@@ -62,7 +64,6 @@ function resultOut(value){
     guessedVal.value = '';
     // prevGuessOutput += `${guess}, `;
     prevGuessOutput.innerHTML = prevGuess
-    numGuess--;
     leftGuessCount.innerHTML = `${numGuess}, `;
 }
 function printErr(message){
@@ -71,4 +72,29 @@ function printErr(message){
     setTimeout(()=>{
         errBox.style.display = "none";
     },3000);
+}
+function displayMessage(mess){
+    message.innerHTML= `<h2 id="newGame">${mess}<h2>`;
+}
+function endGame(){
+    guessedVal.value = '';
+    guessedVal.disabled = true;  // now user can't type anymore
+    p.classList.add('button');
+    p.innerHTML = `<h2 id="newGameButton">Start new Games</h2>`;
+    startOver.appendChild(p);
+    playGame = false;
+    newGame();
+}
+function newGame(){
+    const newGameButton = document.querySelector('#newGameButton');
+    newGameButton.addEventListener('submit',(e)=>{
+        randomNum = Math.trunc(Math.random()*100)+1;
+        prevGuess = [];
+        numGuess = 10;
+        prevGuessOutput.innerHTML = '';
+        leftGuessCount.innerHTML = numGuess;
+        guessedVal.removeAttribute('disabled');
+        startOver.removeChild(p);
+        playGame = true;
+    });
 }
