88e51a0ccab56e8995cb52bdd6304cbb9144c4e6
Stu-me <stusundram@gmail.com>
2025-10-06 19:06:52 +0530

    enter - keys project deploy

---
+++ project5.enterKeys/script.js
@@ -0,0 +1,44 @@
+const clicked = document.querySelector('#resultTable');
+
+window.addEventListener('click', checkInput); //will not work as called before declared - checkinput
+window.addEventListener('keydown', checkInput);
+
+function checkInput(e) {
+    e.preventDefault();
+    if (e.type === 'click') {
+        mouseOutput(e);
+    }
+    else keyboardOutput(e);
+}
+function mouseOutput(e) {
+    clicked.innerHTML = `
+          <table>
+            <tr>
+                <th>What Input Device</th>
+                <th>What Button Pressed</th>
+                <th>Button - Code</th>
+            </tr>
+            <tr>
+                <td>Mouse</td>
+                <td>${e.button}</td>
+                <td>${e.which}</td>
+            </tr>
+            
+        </table>`;
+}
+function keyboardOutput(e) {
+            clicked.innerHTML = `
+          <table>
+            <tr>
+                <th>What Input Device</th>
+                <th>What Button Pressed</th>
+                <th>Button - Code</th>
+            </tr>
+            <tr>
+                <td>KeyBoard</td>
+                <td>${e.key}</td>
+                <td>${e.code}</td>
+            </tr>
+            
+        </table>`;
+}
